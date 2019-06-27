49c2cecab7e2f1f05b319ad38d42f0ba5f542271
ashbellett <ash.bellett.ab@gmail.com>
2019-06-27 10:44:22 +0930

    Documented dynamic array functions.

--- dynamic-array/include/dynamic-array.h
+++ dynamic-array/include/dynamic-array.h
@@ -2,5 +2,7 @@
 #include <stdio.h>
 
 int *create_array(int size);
-void populate_array(int *array, int data);
+void insert_array(int *array, int data);
+void remove_array(int *array, int data);
+void delete_array(int *array);
 void print_array(int *array, int size);
--- dynamic-array/src/dynamic-array.c
+++ dynamic-array/src/dynamic-array.c
@@ -1,16 +1,24 @@
 #include "../include/dynamic-array.h"
 
 int *create_array(int size) {
+    /* Allocates memory for array. */
     return calloc(size, sizeof(int));
 }
 
-void populate_array(int *array, int data) {
+void insert_array(int *array, int data) {
+    /* Inserts data into array. */
     for (int i = 0; i < data; i++) {
         *(array + i) = i;
     }
 }
 
+void delete_array(int *array) {
+    /* Frees array memory. */
+    free(array);
+}
+
 void print_array(int *array, int size) {
+    /* Prints elements in array. */
     for (int i = 0; i < size; i++) {
         printf("%d\n", *(array + i));
     }
