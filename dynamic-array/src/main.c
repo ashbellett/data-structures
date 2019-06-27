e7497751674bd1da833d7a36ed5e5fa41d545fb6
Ash Bellett <abellett@au1.ibm.com>
2019-06-27 10:44:22 +0930

    Documented dynamic array functions.

--- dynamic-array/src/main.c
+++ dynamic-array/src/main.c
@@ -1,12 +1,11 @@
 #include "../include/dynamic-array.h"
 
 int main(void) {
-    int data;
-    int *array;
-    int size = 4;
-    array = create_array(size);
-    populate_array(array, size);
-    print_array(array, size);
-    free(array);
+    int *array;                 // pointer to first element in array
+    int size = 4;               // initial size of array
+    array = create_array(size); // Allocate memory for array
+    insert_array(array, size);  // Insert data into array
+    print_array(array, size);   // Print elements in array
+    delete_array(array);        // Free array memory
     return 0;
 }
