ffd4a0b4f73c3ae5c6129524592cdb9f350dcb15
ashbellett <ash.bellett.ab@gmail.com>
2019-06-28 12:10:12 +0930

    Replaced C implementation with C++.

--- linked-list/include/linked-list.h
+++ linked-list/include/linked-list.h
@@ -1,15 +1,16 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-typedef struct List {
-    /* Linked list structure containing data and pointer to next list. */
-    char *data;        // data item
-    struct List *next; // pointer to next list
-} List;
+class Node {
+    public:
+        int data;   // data element
+        Node *next; // pointer to next node
+        Node();     // constructor
+};
 
-List *create_list(void);
-void insert_list(List **list, char *data);
-List *search_list(List *list, char *data);
-List *previous_list(List *list, char *data);
-void delete_list(List **list, char *data);
-void print_list(List *list);
+void insert(Node **list, int data);
+void remove(Node **list, int data);
+Node *search(Node *list, int data);
+void clear(Node *list);
+void print(Node *list);
+int length(Node *list, int count);
